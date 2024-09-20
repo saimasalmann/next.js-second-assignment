106450c1546c45a286d12225dcf0327ebdf5b3d2
saimasalmann <786saimasos@gmail.com>
2024-09-21 00:37:56 +0500

    push code

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import About from "./components/About";
+
+export default function Home(){
+  return(
+  <>
+    <h1>This is Homepage.</h1>
+    <Header/>
+    <About/>
+    
+    
+    </>
+  )
+};
